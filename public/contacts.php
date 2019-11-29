9225ee8d31911915bb4e308a0dc95a938a07865d
jetsaus <jetsaus@gmail.com>
2019-11-30 00:11:06 +0800

    Мелкие правки по коду

--- public/contacts.php
+++ public/contacts.php
@@ -1,6 +1,6 @@
 <?php
     require_once '../config/config.php';
-    // Отображение шаблона контактов
+    // Отображение контактов
     echo render(TEMPLATES_DIR . 'contacts.tpl', [
         'title'     => 'Контакты',
         'h4'        => 'Мои контакты:',
--- public/gallery.php
+++ public/gallery.php
@@ -1,8 +1,8 @@
 <?php
     require_once '../config/config.php';
-    // Отображение шаболона галереи
-    echo render(TEMPLATES_DIR . 'gallery.tpl', [
+    $htmlGallery = getGallery(IMG_DIR, 4);              // Формирование HTML галереи
+    echo render(TEMPLATES_DIR . 'gallery.tpl', [        // Отображение галереи
             'title'     => 'Фото-зоопарк',
             'head'      => 'Фото-зоопарк:',
-            'content'   => getGallery(IMG_DIR, 4)       // Вызов функции вывода галереи
+            'content'   => $htmlGallery
     ]);
